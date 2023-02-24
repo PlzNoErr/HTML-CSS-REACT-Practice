6f8791a41923f6fda99efa36df90edef8fb7c16d
YeonHak Lee <dldusgkr788@gmail.com>
2023-02-24 17:28:00 +0900

    타입스크립트 연습2

--- 코딩애플_강의/TypeScript강의/연습1.ts
+++ 코딩애플_강의/TypeScript강의/연습1.ts
@@ -5,3 +5,20 @@ let myInfo: { name: string; age: number; home: string } = {
 };
 
 type Project = { member: string[]; days: number; started: boolean };
+
+let user: string = "kim";
+let age: undefined | number = undefined;
+let married: boolean = false;
+let 철수: (string | undefined | number | boolean)[] = [user, age, married];
+
+type School = {
+  score: (number | boolean)[];
+  teacher: string;
+  friend: string[] | string;
+};
+
+let 학교: School = {
+  score: [100, 97, 84],
+  teacher: "Phil",
+  friend: "John",
+};
