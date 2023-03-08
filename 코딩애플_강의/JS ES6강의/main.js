f1cec8c6093a75f10b66960dac629efaba8641e5
YeonHak Lee <dldusgkr788@gmail.com>
2023-03-08 22:28:59 +0900

    optional Chaining

--- 코딩애플_강의/JS ES6강의/main.js
+++ 코딩애플_강의/JS ES6강의/main.js
@@ -32,23 +32,55 @@ class Cactus {
 
 let timer = 0;
 let cactusArr = [];
+let jumping = false;
+let jumpTimer = 0;
+let animation;
+
+document.addEventListener("keydown", function (e) {
+  if (e.code == "Space") jumping = true;
+});
 
 function 프레임마다실행() {
-  requestAnimationFrame(프레임마다실행);
+  animation = requestAnimationFrame(프레임마다실행);
   timer++;
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  if (timer % 150 === 0) {
+  if (timer % 120 === 0) {
     let cactus = new Cactus();
     cactusArr.push(cactus);
   }
-  cactusArr.forEach((a) => {
-    a.x--;
+  cactusArr.forEach((a, i, o) => {
+    if (a.x < -50) o.splice(i, 1);
+    a.x -= 7;
+
+    crashCheck(dino, a);
+
     a.draw();
   });
 
+  if (jumping) {
+    dino.y -= 5;
+    jumpTimer++;
+  }
+  if (!jumping) {
+    if (dino.y < 200) dino.y += 5;
+  }
+
+  if (jumpTimer > 20) {
+    jumping = false;
+    jumpTimer = 0;
+  }
+
   dino.draw();
+}
+function crashCheck(dino, cactus) {
+  let x차이 = cactus.x - (dino.x + dino.width);
+  let y차이 = cactus.y - (dino.y + dino.height);
+  if (x차이 < 0 && y차이 < 0) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    cancelAnimationFrame(animation);
+  }
 }
 
 프레임마다실행();
